7b72cf6e682be359bbe6d300ec3ff164d82a5b81
Shivam Upadhyay <shivamup4545@gmail.com>
2023-06-21 22:57:03 +0530

    first design commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Register from './pages/auth/Register';
+import Cart from './pages/Cart';
+import CheckOut from './pages/CheckOut';
+
+ const router = createBrowserRouter([
+   {
+     path: "/",
+     element: <Home />,
+   },
+   {
+    path:'cart',
+    element:<Cart/>
+   },
+   {
+    path:'checkout',
+    element:<CheckOut/>
+   },
+   {
+     path: "login",
+     element: <Login />,
+   },
+   {
+     path: "register",
+     element: <Register />,
+   },
+ ]);
 
 function App() {
+ 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router} />
     </div>
   );
 }
